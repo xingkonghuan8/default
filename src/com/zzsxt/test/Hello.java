603b670f81688dbd1b5f37b72638f39f34350c68
xingkonghuan8 <46293795+xingkonghuan8@users.noreply.github.com>
2019-01-01 17:10:41 +0800

    Update Hello.java

--- src/com/zzsxt/test/Hello.java
+++ src/com/zzsxt/test/Hello.java
@@ -3,11 +3,11 @@ package com.zzsxt.test;
 import com.sun.org.apache.xpath.internal.operations.String;
 
 /**
- * Created by Administrator on 2018/10/8 0008.
+ * Created 
  */
 public class Hello {
   public static void main(String[] args){
- System.out.println(add(5,6));
+ System.out.println(add(4,6));
 
 
    }
