c3688a48ab7743d4016570d58dc99957f26cd88e
haowenxiang <18903820762@163.com>
2019-01-01 17:03:49 +0800

    第一个

---
+++ src/com/zzsxt/test/Hello.java
@@ -0,0 +1,17 @@
+package com.zzsxt.test;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+/**
+ * Created by Administrator on 2018/10/8 0008.
+ */
+public class Hello {
+  public static void main(String[] args){
+ System.out.println(add(5,6));
+
+
+   }
+    public static int add(int a,int b){
+        return a+b;
+    }
+}
