f6a2e7fd9e120ea264d44e0495bf6f095551f312
haowenxiang <18903820762@163.com>
2019-01-02 11:08:36 +0800

    第一次改变

--- src/com/zzsxt/test/Hello.java
+++ src/com/zzsxt/test/Hello.java
@@ -2,16 +2,12 @@ package com.zzsxt.test;
 
 import com.sun.org.apache.xpath.internal.operations.String;
 
-/**
- * Created 
- */
+
 public class Hello {
   public static void main(String[] args){
- System.out.println(add(4,6));
+ System.out.println(56);
 
 
    }
-    public static int add(int a,int b){
-        return a+b;
-    }
+
 }
